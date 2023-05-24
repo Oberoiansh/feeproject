ce0510b3d937b30cb7f190a46542d3cb83da2b96
Oberoiansh <123056477+Oberoiansh@users.noreply.github.com>
2023-05-24 21:56:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let toastBox = document.querySelector(".toast");
 
-let msg=["hi","hello","hola"]
+let msg=["https://youtu.be/51VupvTTSno","https://youtu.be/ewwxbpJecx4","https://youtu.be/pSK7eNh1r9E","https://youtu.be/r0PhmnYo-us","https://youtu.be/BLUh8QCLoSU","https://youtu.be/nAIFCWMhC_0","https://youtu.be/UeTDUClwXsE"]
 
 function append1() {
   let toast = document.createElement("a");
-  let a=Math.floor(Math.random() * 3)
+  let a=Math.floor(Math.random() * msg.length)
   toast.href=msg[a]
-  toast.innerHTML = msg[a];
+  toast.target="_blank"
+  toast.innerHTML = "notification: click here";
   toast.classList.add("notification");
   toastBox.appendChild(toast);
 
-  setTimeout(() => {
+  setTimeout(function(){
     toast.remove();
-  }, 5000);
+  }, 10000);
 }

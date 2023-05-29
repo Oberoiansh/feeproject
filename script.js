c3a49bf016025f69d08cdd38c15ba15bd23b57f1
Oberoiansh <123056477+Oberoiansh@users.noreply.github.com>
2023-05-29 17:42:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 let toastBox = document.getElementById("toastBox");
 let section =
-  'You Are Touching My Eyes 😊';
+  ['You Are Touching My Eyes 😊',new Audio("eyes.mp3")];
 let team_member =
-  'You Are Touching My Nose 😒';
+  ['You Are Touching My Nose 😒',new Audio("nose.mp3")];
 let project =
-  'You Are Touching My Tongue 😝';
-let bell='You Are Touching My Bell 😍'
-let belt="You Are Touching My Belt 😁"
-let face="You Are Touching My Face 😂"
+  ['You Are Touching My Tongue 😝',new Audio("tounge.mp3")];
+let bell =
+  ['You Are Touching My Bell 😍',new Audio("BELL.mp3")];
+let belt =
+  ['You Are Touching My Belt 😁',new Audio("BELT.mp3")];
+let face =
+  ['You Are Touching My Face 😂',new Audio("head (2).mp3")];
 
 function showToast(msg) {
   let toast = document.createElement("div");
   toast.classList.add("toast");
-  toast.innerHTML = msg;
+  toast.innerHTML = msg[0];
   toastBox.appendChild(toast);
+  msg[1].play()
 
-  if (msg.includes("Aman")) {
-    toast.classList.add("team");
-  }
-  if (msg.includes("toast")) {
-    toast.classList.add("project");
-  }
 
   setTimeout(() => {
     toast.remove();
